e8eb7117691d0e861ad5439d757394d48ccd2da6
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-09-20 10:16:12 +0545

    project initialized

---
+++ aws-s3-file-upload/src/test/java/com/kabin/awss3/AwsS3FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabin.awss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
